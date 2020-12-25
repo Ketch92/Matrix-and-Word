120ef44f2aae46c0d24c5b9c96c6d3105e1b4883
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-25 17:50:36 +0200

    small refactoring again

--- src/test/java/MatrixAndWordTest.java
+++ src/test/java/MatrixAndWordTest.java
@@ -8,7 +8,7 @@ class MatrixAndWordTest {
     @Test
     void twoNullValuesInputExceptionExpected() {
         Assertions.assertThrows(IOException.class,
-                () -> new MatrixAndWord(null, null));
+                MatrixAndWord::new);
     }
     
     @Test
